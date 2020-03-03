d569670cfa209b0bfb579fe30dfbeac60f77ce02
Dante J. Anjos <dantewebmaster@MBP-de-Dante.home>
2020-03-03 01:28:02 -0300

    Configured icons + Application name

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 
 // Import parts of electron to use
-const { app, BrowserWindow, shell } = require('electron');
+const {
+  app, BrowserWindow, shell, Menu,
+} = require('electron');
 const path = require('path');
 const url = require('url');
 const { default: installExtension, REACT_DEVELOPER_TOOLS } = require('electron-devtools-installer');
@@ -23,6 +25,90 @@ if (process.platform === 'win32') {
   app.commandLine.appendSwitch('force-device-scale-factor', '1');
 }
 
+const isMac = process.platform === 'darwin';
+const template = [
+  ...(isMac ? [{
+    label: app.name,
+    submenu: [
+      { role: 'services' },
+      { type: 'separator' },
+      { role: 'hide' },
+      { role: 'hideothers' },
+      { role: 'unhide' },
+      { type: 'separator' },
+      { role: 'quit' },
+    ],
+  }] : []),
+  {
+    label: 'File',
+    submenu: [
+      isMac ? { role: 'close' } : { role: 'quit' },
+    ],
+  },
+  {
+    label: 'Edit',
+    submenu: [
+      { role: 'undo' },
+      { role: 'redo' },
+      { type: 'separator' },
+      { role: 'cut' },
+      { role: 'copy' },
+      { role: 'paste' },
+      ...(isMac ? [
+        { role: 'pasteAndMatchStyle' },
+        { role: 'delete' },
+        { role: 'selectAll' },
+        { type: 'separator' },
+        {
+          label: 'Speech',
+          submenu: [
+            { role: 'startspeaking' },
+            { role: 'stopspeaking' },
+          ],
+        },
+      ] : [
+        { role: 'delete' },
+        { type: 'separator' },
+        { role: 'selectAll' },
+      ]),
+    ],
+  },
+  {
+    label: 'View',
+    submenu: [
+      { role: 'reload' },
+      { role: 'forcereload' },
+      { role: 'togglefullscreen' },
+    ],
+  },
+  {
+    label: 'Window',
+    submenu: [
+      { role: 'minimize' },
+      ...(isMac ? [
+        { type: 'separator' },
+        { role: 'front' },
+      ] : [
+        { role: 'close' },
+      ]),
+    ],
+  },
+  {
+    role: 'help',
+    submenu: [
+      {
+        label: 'Author',
+        click: async () => {
+          await shell.openExternal('https://github.com/dantewebmaster');
+        },
+      },
+    ],
+  },
+];
+
+const menu = Menu.buildFromTemplate(template);
+Menu.setApplicationMenu(menu);
+
 function createWindow() {
   // Create the browser window.
   mainWindow = new BrowserWindow({
@@ -31,10 +117,13 @@ function createWindow() {
     minWidth: 850,
     minHeight: 691,
     show: false,
+    icon: path.join(__dirname, 'src/assets/icons/png/64x64.png'),
     webPreferences: {
       nodeIntegration: true,
+      devTools: false,
     },
   });
+
 
   // and load the index.html of the app.
   let indexPath;
